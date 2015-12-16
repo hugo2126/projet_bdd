b18a3fc7c69dd8928202a7fc546328a9f97ce6c9
hugo2126 <hugo_dml@hotmail.fr>
2015-12-16 23:56:15 +0100

    Projet de BDD
    Salut

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/fichier.css" />
+		<link rel="stylesheet" href="css/font-awesome.css" />
+        <title></title>
+    </head>
+
+    <body>
+        
+		<div id="header">
+		
+			<div id="titre">
+				<i class="fa fa-calendar"></i> &nbsp &nbsp &nbsp &nbsp &nbsp Community Events &nbsp &nbsp &nbsp &nbsp &nbsp <i class="fa fa-male"></i><i class="fa fa-female"></i>
+			</div>
+			
+			<br>
+			
+			<ul id="menu-deroulant">
+			<li><a href="#">Lien menu 1</a>
+			<ul>
+			<li><a href="#">lien sous menu 1</a></li>
+			<li><a href="#">lien sous menu 1</a></li>
+			<li><a href="#">lien sous menu 1</a></li>
+			<li><a href="#">lien sous menu 1</a></li>
+			</ul>
+			</li>
+			<li><a href="#">Lien menu 2</a>
+			<ul>
+			<li><a href="#">Lien sous menu 2</a></li>
+			<li><a href="#">Lien sous menu 2</a></li>
+			<li><a href="#">Lien sous menu 2</a></li>
+			<li><a href="#">Lien sous menu 2</a></li>
+			</ul>
+			</li>
+			</ul>
+			</div>
+  
+		<div id="footer">
+		<!-- Ton footer -->
+		</div>
+
+    </body>
+</html>
